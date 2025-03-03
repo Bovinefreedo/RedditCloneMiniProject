cc2da76ea24652fcf1cd3f907ff386e5d043cd7e
BovineFreedo <frankskorstengaard@gmail.com>
2025-03-03 10:04:12 +0100

    I have made a DbContext in the entity framework

--- RedditCloneMiniProjectAPI/Model/Post.cs
+++ RedditCloneMiniProjectAPI/Model/Post.cs
@@ -4,6 +4,7 @@
     {
         public int PostId { get; set; }
         public string Title { get; set; } = string.Empty;
+        public string Content {  get; set; } = string.Empty;
         public User? Author { get; set; }
         public List<Comment> comments { get; set; } = new();
         public int score = 0;
---
+++ RedditCloneMiniProjectAPI/Model/PostContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedditCloneMiniProjectAPI.Model
+{
+    public class PostContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public string DbPath { get; }
+
+        public PostContext() {
+            DbPath = "bin/Post.db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+         => options.UseSqlite($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().ToTable("Posts");
+        }
+    }
+}
